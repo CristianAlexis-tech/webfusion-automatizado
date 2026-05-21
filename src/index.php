c70ac5074ecd00121926083c8b90c7d37dba5094
Alexis Hernández <alexis@gmail.com>
2026-05-21 12:58:33 +0200

    actualizacion php

--- src/index.php
+++ src/index.php
@@ -2,5 +2,6 @@
 echo "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>";
 echo "<h1>Bienvenidos a WebFusion Digital</h1>";
 echo "<p>Versión 1.0 - Despliegue Automatizado Inicial</p>";
+echo "<h1>Modificacion desde local el php</h1>";
 echo "</div>";
 ?>
